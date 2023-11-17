6c966c880d115927a09f811933e8058be173adf0
Maysam <maysam.shahsavari@dakiacan.com>
2023-11-16 20:31:01 -0500

    Adding a networking service

---
+++ TDD App/TDD_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD_AppApp.swift
+//  TDD App
+//
+//  Created by Maysam Shahsavari on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct TDD_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
