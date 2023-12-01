3f763c9c4439b720863334297c39ece89ac6fa18
Maysam <maysam.shahsavari@gmail.com>
2023-11-30 22:12:02 -0500

    Updated the url for the sample images

--- TDD App/Models/RemoteAssets.swift
+++ TDD App/Models/RemoteAssets.swift
@@ -9,5 +9,5 @@ import Foundation
 
 struct RemoteAssets {
     static let extraData = "https://gist.githubusercontent.com/maysamsh/c539e299b061591a1e316f0fcac598b2/raw/a04d04b0341c5c5621c5254950c2e49f42c24539/extra-data-sample.json"
-    static let images = "https://gist.github.com/maysamsh/bd3b57b4bd9266de24bfc3203fc5f85b/raw/9df83e13163d1aed04c4dc6be68264ad6ca6cda7/images-sample.json"
+    static let images = "https://gist.githubusercontent.com/maysamsh/bd3b57b4bd9266de24bfc3203fc5f85b/raw/eb62b397151246e1b0394859eb7f39ecb0c9a026/images-sample.json"
 }
